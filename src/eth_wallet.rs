75266232d5af466eb2a0d41f69e68a49da091a5b
Jason Schwarz <jason.c.schwarz@gmail.com>
2023-06-07 19:55:47 -0300

    Saving wallet information to a file

--- src/eth_wallet.rs
+++ src/eth_wallet.rs
@@ -1,4 +1,8 @@
+use anyhow::Result;
 use secp256k1::{ rand::{ rngs, SeedableRng }, PublicKey, SecretKey };
+use serde::{ Deserialize, Serialize };
+use std::io::BufWriter;
+use std::fs::OpenOptions;
 use tiny_keccak::keccak256;
 use web3::types::Address;
 
@@ -15,4 +19,31 @@ pub fn public_key_address(public_key: &PublicKey) -> Address {
     let hash = keccak256(&public_key[1..]);
 
     Address::from_slice(&hash[12..])
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Wallet {
+    pub secret_key: String,
+    pub public_key: String,
+    pub public_address: String,
+}
+
+impl Wallet {
+    pub fn new(secret_key: &SecretKey, public_key: &PublicKey) -> Self {
+        let addr: Address = public_key_address(&public_key);
+        Wallet {
+            secret_key: secret_key.to_string(),
+            public_key: public_key.to_string(),
+            public_address: format!("{:?}", addr),
+        }
+    }
+
+    pub fn save_to_file(&self, file_path: &str) -> Result<()> {
+        let file = OpenOptions::new().write(true).create(true).open(file_path)?;
+        let buf_writer = BufWriter::new(file);
+
+        serde_json::to_writer_pretty(buf_writer, self)?;
+
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use anyhow::Result;
 mod eth_wallet;
-fn main() {
+fn main() -> Result<()> {
     let (secret_key, pub_key) = eth_wallet::generate_keypair();
 
     println!("secret key: {}", &secret_key.to_string());
@@ -7,4 +8,11 @@ fn main() {
 
     let pub_address = eth_wallet::public_key_address(&pub_key);
     println!("public address: {:?}", pub_address);
+
+    let crypto_wallet = eth_wallet::Wallet::new(&secret_key, &pub_key);
+    println!("crypto_wallet: {:?}", &crypto_wallet);
+
+    crypto_wallet.save_to_file("crypto_wallet.json")?;
+
+    Ok(())
 }
